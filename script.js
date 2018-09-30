b44b975f06064f794c3b20c21365d6c9aabedc94
keerthanapareddy <skp440@nyu.edu>
2018-09-30 16:05:47 -0400

    unique dates in array

--- script.js
+++ script.js
@@ -12,60 +12,42 @@ var n = 0;
 $.getJSON('/locationHistory.json', function(data) {
   console.log(data.locations.length);
 
-  //d3.extent accepts an array of values and returns the boundary(min and max) as an array.
+    //d3.extent accepts an array of values and returns the boundary(min and max) as an array.
     var bounds = d3.extent(data.locations, function (d){
       return d.timestampMs;
     });
 
-    // console.log(new Date(+bounds[0]), new Date(+bounds[1]));
-    // var date = JSON.stringify(new Date(+bounds[0]));
-    // console.log("stringified date");
+    console.log(new Date(+bounds[0]), new Date(+bounds[1]));
+    var date = JSON.stringify(new Date(+bounds[0]));
+    console.log(date);
     // console.log(date.slice(0,11));
 
       //////GROUP SAME DATES
-      var results = [];
+      let uniqueDateSet = new Set(); //set automatically eliminates repitition
+      var uniqueDates = [];
 
       for (var i = 0; i < data.locations.length - 1; i++) {
-          var dateStringified = JSON.stringify(new Date(+data.locations[i].timestampMs));
-          var dateOnly = dateStringified.slice(0,11); //gives only the first 11 characters of the stringify
+          var readableDateTime = JSON.stringify(new Date(+data.locations[i].timestampMs));
+          var readableDate = readableDateTime.slice(1,11); //gives only the first 11 characters of the stringify
                                                     //in this case it gives me just the date.
+          var readableTime = readableDateTime.slice(12,20); //for future: slice it after the letter T, not characters
 
-          // console.log(dateOnly);
-      }
-
-
-
-// console.log(results);
-
-/*
-    for(let i = 0; i < data.locations.length; i++){
-      //have i
-      //compare it to i+n
-      //start with n = 0;
-      //n++
-      var dateI = JSON.stringify(new Date(+data.locations[i].timestampMs));
-      var dateIN = JSON.stringify(new Date(+data.locations[i + n].timestampMs));
-      console.log(dateIN);
-
-      if(dateI == dateIN){
-        console.log("Same Dates");
-        console.log(dateI);
-
-        // console.log(+data.locations[i].timestampMs);
+          uniqueDateSet.add(readableDate);
+          //if the date occurs more than once, display it only once
 
       }
-      n++;
-      console.log(n);
 
-    }
 
-*/
+      //push unique dates into an array
+      uniqueDateSet.forEach(function(uniqueDate){
+        uniqueDates.push(uniqueDate); //is an array of 143 elements with unique dates in it
+      });
+        console.log(uniqueDates);
 
 
 
 
     //keep a track of same locations in that day. for eg: if multiple timestamps have the same location coordinates, consider it as one location
-
 
 
     var time = d3.scaleTime().domain();
