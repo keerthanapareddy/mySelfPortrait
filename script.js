b8da6113a75c435c4a027ccf73545d8f71da3a24
keerthanapareddy <skp440@nyu.edu>
2018-10-22 15:21:09 -0400

    object with dates and coordinates

--- script.js
+++ script.js
@@ -8,6 +8,8 @@
 
 
 var n = 0;
+var divisor = 10000000;
+var distanceUnit = {units: 'kilometers'};
 
 $.getJSON('/locationHistory.json', function(data) {
   console.log(data.locations.length);
@@ -22,30 +24,82 @@ $.getJSON('/locationHistory.json', function(data) {
 
     var datesWRepitition = []; //array of all the dates
 
-    for (var i = 0; i < data.locations.length - 1; i++) {
-      var readableDateTime = JSON.stringify(new Date(+data.locations[i].timestampMs));
-      var readableDate = readableDateTime.slice(1,11); //gives only the first 11 characters of the stringify
-                                                    //in this case it gives me just the date.
-      var readableTime = readableDateTime.slice(12,20); //for future: slice it after the letter T, not characters
-      datesWRepitition.push(readableDate); //array of all the stringified dates with repetition
+    var coordinates = [];
+
+
+    for (var i = 0; i < data.locations.length; i++) {
+        var readableDateTime = JSON.stringify(new Date(+data.locations[i].timestampMs));
+        var readableDate = readableDateTime.slice(1,11); //gives only the first 11 characters of the stringify
+                                                      //in this case it gives me just the date.
+        var readableTime = readableDateTime.slice(12,20); //for future: slice it after the letter T, not characters
+        datesWRepitition.push(readableDate); //array of all the stringified dates with repetition
+
+        var coordinatesArr = [];
+        var latitudeConverted = data.locations[i].latitudeE7/divisor;
+        var longitudeConverted = data.locations[i].longitudeE7/divisor;
+        // console.log(latitudeConverted, longitudeConverted);
+        coordinatesArr[0] = latitudeConverted;
+        coordinatesArr[1] = longitudeConverted;
+        coordinates.push(coordinatesArr);
     }
 
-    // console.log(datesWRepitition);
+      var mapsHistory ={
+        history:[]
+      };
 
-//baracks code
+    datesWRepitition.map(d => {
+      mapsHistory.history.push({
+        date : d
+      })
+    })
+
+
+    for(i = 0;i < mapsHistory.history.length; i++){
+      mapsHistory.history[i].coordinates = coordinates[i];
+    }
+
+    console.log(mapsHistory.history);
+
+    var hyderabadHome = turf.point([17.5000, 78.3856]);
+    var cincyHome1 = turf.point([39.134548, -84.510592]);
+    var cincyHome2 = turf.point([43.881638, -85.797401]);
+
+    for(i = 0;i < mapsHistory.history.length; i++){
+      var datesSliced = mapsHistory.history[i].date.slice(0,7);
+      if( datesSliced == '2018-08' || datesSliced == '2018-07'){
+        var count = 0;
+        var distanceCincyHome1 = turf.distance(cincyHome2, turf.point(mapsHistory.history[i].coordinates), distanceUnit);
+        // console.log(distanceCincyHome1);
+        if(distanceCincyHome1 < 2){ //if distance b/w my home coordinates and the array coordinates is less then 2km,
+          mapsHistory.history.splice(i,1); // remove those from the array
+        }
+      }
+    }
+
+      console.log(mapsHistory.history);
+
+
+    //if the key exists add 1 to the value otherwise create the key and set it to 0
     var uniqueDatesCount = {};
-    //extract unique dates with no repition from the array of datesWRepitition
+    //extract unique dates with no repitition from the array of datesWRepitition
     for(var i = 0; i < datesWRepitition.length ; i++){
-      uniqueDatesCount[datesWRepitition[i]] = uniqueDatesCount[datesWRepitition[i]] || 0;
-      uniqueDatesCount[datesWRepitition[i]]++;
+      if (uniqueDatesCount.hasOwnProperty([datesWRepitition[i]])){
+        // console.log(uniqueDatesCount[datesWRepitition[i]])
+        uniqueDatesCount[datesWRepitition[i]]++
+      }else{
+        uniqueDatesCount[datesWRepitition[i]] = 0;
+      }
     }
 
+
+
+//creating new object to push clean data
     var newLocationHistory = {  //new empty object with an array of days
-      days: []
+      locationData: []
     };
 
     Object.keys(uniqueDatesCount).map(d => {
-    newLocationHistory.days.push({
+    newLocationHistory.locationData.push({
       date: d   //pushing key and value pairs of dates that is mapped with uniqueDatesCount
     })
   })
@@ -60,105 +114,26 @@ $.getJSON('/locationHistory.json', function(data) {
     })
   })
 
-
-for(i=0;i<newLocationHistory.days.length; i++){
-  newLocationHistory.days[i].count = counts.count[i].count
-
+for(i=0;i<newLocationHistory.locationData.length; i++){
+  newLocationHistory.locationData[i].count = counts.count[i].count;
   //newLocationHistory.days[i].count creates a new key "count"
   //and to each i we are assigning i of the count array
 }
 
-  console.log(newLocationHistory.days);
-  var data = newLocationHistory;
+
+  console.log(newLocationHistory);
 
 
-  var maxCount = d3.max(data.days,function(d){return d.count;});
-  var minCount = d3.min(data.days,function(d){return d.count;});
+  // var divisor = 10000000; lat and log divide by divisor/
+  // make lat and long into a pair. array inside an array. coordinates
+  //Date.prototype.getDay() to get the day
+  //eliminate same location: turf.distance if less than threshold, consider same locationHistory
+  //for every same location, count and draw one circle
+  //date object js find 0-6 weekdays, use this for color.
 
 
-/*
-  var time = d3.scaleLinear()
-              .domain([2, 693])
-              .range([0, 20]);
 
-  var svg = d3.select("svg"),
-      margin = {top: 20, right: 20, bottom: 30, left: 40},
-      width = +svg.attr("width") - margin.left - margin.right,
-      height = +svg.attr("height") - margin.top - margin.bottom;
-
-  var x = d3.scaleLinear()
-            .range([0, width - (margin.left + margin.right)]);
-
-  var y = d3.scaleLinear()
-          .range([height - (margin.top * 2), 0]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10, "");
-
-  var line = d3.line()
-              .x(function(d) { return x(d.date); })
-              .y(function(d) { return y(d.count); });
-/*
-  d3.data(data,functions(error,data){
-    if (error) throw error;
-
-      data.forEach(function(d) {
-        d.date = +d.date;
-        d.count = +d.count;
-      });
-
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-
-
-/*
-  var svgContainer = d3.select("#inner").append("svg")
-                      .attr("width", 1200)
-                      .attr("height",300)
-
-  var circles = svgContainer.selectAll('circle')
-                            .data(data.days)
-                            .enter()
-                            .append("circle");
-
-  var circleAttributes = circles
-                        .attr("r",10)
-                        .attr("cx",function (d) { return d.days.count; })
-                        .attr("cy", 500);
-
-                        // .style("fill", #000000);
-*/
-
-  // d3.select('#inner')
-  //   .selectAll('circle')
-  //   .data(data.days)
-  //   .enter()
-  //   .append('circle')
-  //   .attr('r', 5)
-  //   .attr('cx', function(d) {
-  //     return time(d);
-  //     })
-  //   // .attr('cx',5)
-  //   .attr('cy', 10);
 
 
 
 });
-
-
-
-
-//are they sorted in any order?
-//sort as an integer and not a string
-
-//find the location that has occured max number of times.
-//this location is your home(A) or office(B)
-
-//if you have travelled to a location other than A and B, draw a line
-
-//also find how many locations for each day..
